1394e8333d207b38a0ad495769cdc53023e75d76
Devesh Jain <deveshsolapur@gmail.com>
2022-05-16 05:46:39 +0530

    1st update

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var inputWeight = parseInt($("#weight").val());
+    var inputHeight = parseInt($("#height").val());
+    var inputWidth = parseInt($("#width").val());
+    var inputDepth = parseInt($("#depth").val());
+    var inputDist = parseInt($("#dist").val());
+    var box = { weight: inputWeight, height: inputHeight, width: inputWidth, depth: inputDepth, dist:inputDist };
+    if (inputWeight && inputHeight && inputWidth && inputDepth && inputDist) {
+      var outputCost = (1.5*box.height*box.width*box.depth + .5*box.weight)*box.dist/200;
+      $("#cost-output").fadeIn();
+      $(".cost").text(outputCost);
+    } else {
+      $("#cost-output").hide();
+      $(".displayError").fadeIn();
+    }
+  });
+});
